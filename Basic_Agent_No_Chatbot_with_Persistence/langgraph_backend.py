e233b8614c931ef56ef9d9e1a135a5cc28f9e79f
Priyanuj Misra <147880991+priyanuj7@users.noreply.github.com>
2025-08-15 22:50:52 +0400

    Add files via upload

---
+++ Basic_Agent_No_Chatbot_with_Persistence/langgraph_backend.py
@@ -0,0 +1,36 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver, InMemorySaver
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+llm = ChatOpenAI()
+
+# response = llm.invoke("Which model of GPT are you intrinsically using?").content
+# print(response)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+
+chatbot = graph.compile(checkpointer=checkpointer)
